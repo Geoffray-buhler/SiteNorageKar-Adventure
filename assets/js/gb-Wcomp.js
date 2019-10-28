e542a9cca5694f18b1b206c859fdc02d69121ce4
BUHLER Geoffray <geoffray.buhler@gmail.com>
2019-10-28 14:12:40 +0100

    Ajout d'un bouton pour le personnage

--- assets/js/gb-Wcomp.js
+++ assets/js/gb-Wcomp.js
@@ -1,5 +1,3 @@
-
-
 // On créé un custom Element pour notre navbar, basé sur bootstrap
 
 // class GbNavBar extends HTMLElement {
@@ -28,20 +26,17 @@
 
 // customElements.define('gb-navbar', GbNavBar);
 
-// definition des bouleens pour les direction de deplacement du personnage
+// Definition des bouleens pour les direction de deplacement du personnage
 
 var actions = {
     "up": false,
     "down": false,
     "left": false,
     "right": false,
-    "shoot_down": false,
-    "shoot_up": false,
-    "shoot_right": false,
-    "shoot_left": false
+    "shoot": false,
 };
 
-// valeur des direction pour la table des sprite personnages
+// Valeur des direction pour la table des sprite personnages
 
 var DIRECTION = {
     "HAUT": 8,
@@ -50,14 +45,10 @@ var DIRECTION = {
     "DROITE": 11
 }
 
-var ACTIONSPLAY = {
-    "SHOOT_HAUT": 1,
-    "SHOOT_GAUCHE": 2,
-    "SHOOT_BAS": 3,
-    "SHOOT_DROITE": 4,
-}
+var deplacementOk ;
+deplacementOk = true ;
 
-// creation du personnage
+// Creation du personnage
 
 function Personnage(pictureFileName, x, y, direction) {
     this.x = x; // (en cases)
@@ -76,10 +67,10 @@ function Personnage(pictureFileName, x, y, direction) {
         this.referenceDuPerso.largeur = this.width / 13;
         this.referenceDuPerso.hauteur = this.height / 21;
     }
-        // lien sur l'image
-        this.image.src = ASSETS_PATH + pictureFileName;
+    // lien sur l'image
+    this.image.src = ASSETS_PATH + pictureFileName;
 
-        // configuration des deplacement du personnage
+    // Configuration des deplacement du personnage
 
     this.keyFrame = 0;
     this.framesPerKeyFrame = 7;
@@ -96,52 +87,55 @@ Personnage.prototype.dessinerPersonnage = function (context) {
         this.largeur, this.hauteur // Taille du rectangle destination (c'est la taille du personnage)
     );
 }
-    // fonction pour lancer des action tout les ticks 60 ticks par seconde
+// fonction pour lancer des actions tout les ticks 60 ticks par seconde
 Personnage.prototype.tick = function () {
     this.deplacement();
-    this.updateMovementAnimation();
+    this.actionsInGame();
 }
-    // systeme de deplacement du personnage en fonction des inputes
+// systeme de deplacement du personnage en fonction des inputes
 Personnage.prototype.deplacement = function () {
-    if (actions.down) {
-        this.y = this.y + this.movementSpeed;
-        this.direction = DIRECTION.BAS;
-    } else if (actions.up) {
-        this.y = this.y - this.movementSpeed;
-        this.direction = DIRECTION.HAUT;
-    } else if (actions.right) {
-        this.x = this.x + this.movementSpeed;
-        this.direction = DIRECTION.DROITE;
-    } else if (actions.left) {
-        this.x = this.x - this.movementSpeed;
-        this.direction = DIRECTION.GAUCHE;
-    }else if (actions.shoot) {
-        coummeca = this.direction;
-    }else if (!this.isMoving()) {
-        this.lol = 0;
+    if (deplacementOk) {
+        if (actions.down) {
+            this.y = this.y + this.movementSpeed;
+            this.direction = DIRECTION.BAS;
+        } else if (actions.up) {
+            this.y = this.y - this.movementSpeed;
+            this.direction = DIRECTION.HAUT;
+        } else if (actions.right) {
+            this.x = this.x + this.movementSpeed;
+            this.direction = DIRECTION.DROITE;
+        } else if (actions.left) {
+            this.x = this.x - this.movementSpeed;
+            this.direction = DIRECTION.GAUCHE;
+        } else if (!this.isMoving()) {
+            this.lol = 0;
+        }
+        this.updateMovementAnimation();
     }
 };
 
-Personnage.prototype.actionsingame = function(){
-    if (action.shoot_down){
-        this.direction = ACTIONSPLAY.SHOOT_BAS;
+Personnage.prototype.actionsInGame = function () {
+    if (actions.shoot) {
+        let directionAtThisMoments = this.direction;
+        this.direction = directionAtThisMoments - 7;
+        this.updateMovementAnimation();
     }
 }
 
 // fonction qui permet de verifier si le personnage bouge 
 
-Personnage.prototype.isMoving = function(){
+Personnage.prototype.isMoving = function () {
     // return !(!actions.left && !actions.up && !actions.right && !actions.down);
     return actions.left || actions.up || actions.right || actions.down || actions.shoot;
 }
 
-Personnage.prototype.updateMovementAnimation = function(){
-    if(!this.isMoving()) return;
+Personnage.prototype.updateMovementAnimation = function () {
+    if (!this.isMoving()) return;
 
     this.lol = 64 * (this.keyFrame % 9);
 
     ++this.animatedFrames;
-    if(this.animatedFrames == this.framesPerKeyFrame){
+    if (this.animatedFrames == this.framesPerKeyFrame) {
         ++this.keyFrame;
         this.animatedFrames = 0;
     }
@@ -151,25 +145,16 @@ var canvasSize = {
     x: 1920,
     y: 888
 }
-
-var canevas = document.getElementById('canvas'); // dans votre HTML, cet élément apparaît comme <canvas id="monCanevas"></canvas>
-
-// {
-//     // Récupère et affiche taille du canvas
-//     const rect = canevas.getBoundingClientRect();
-//     console.log(rect);
-// }
+// dans votre HTML, cet élément apparaît comme <canvas id="monCanevas"></canvas>
+var canevas = document.getElementById('canvas');
 
 var ctx = canevas.getContext('2d');
 var personnages = [];
 
-// personnages.push(new Personnage("telechargement.png", 21.5, 0.6, DIRECTION.BAS));
 personnages.push(new Personnage("telechargement.png", 14.5, 2.3, DIRECTION.BAS));
 
 
 function clear() {
-    // ctx.fillStyle = "transparent";
-    // ctx.fillRect(0, 0, canvasSize.x, canvasSize.y);
     ctx.clearRect(0, 0, canvasSize.x, canvasSize.y);
 }
 
@@ -204,14 +189,9 @@ document.addEventListener("keydown", (evt) => {
         actions.left = true;
     } else if (evt.key === "ArrowRight") {
         actions.right = true;
-    } else if (evt.key === "s") {
-        action.shoot_down = true;
-    } else if (evt.key === "q") {
-        action.shoot_left = true;
-    } else if (evt.key === "d") {
-        action.shoot_right = true;
-    } else if (evt.key === "z") {
-        action.shoot_up = true;
+    } else if (evt.key === "Control") {
+        actions.shoot = true;
+        deplacementOk = false;
     }
 });
 
@@ -224,13 +204,8 @@ document.addEventListener("keyup", (evt) => {
         actions.left = false;
     } else if (evt.key === "ArrowRight") {
         actions.right = false;
-    }else if (evt.key === "s") {
-        action.shoot_down = false;
-    }else if (evt.key === "d") {
-        action.shoot_right = false;
-    }else if (evt.key === "q") {
-        action.shoot_left = false;
-    }else if (evt.key === "z") {
-        action.shoot_up = false;
+    } else if (evt.key === "Control") {
+        actions.shoot = false;
+        deplacementOk = true;
     }
 });
